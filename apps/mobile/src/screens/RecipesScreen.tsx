b737fbe576f360927a2dac1ba8e96bfdfdc94f1d
Michal Kulesza <kulesza.michal@gmail.com>
2026-07-08 14:19:23 +0200

    Use brighter systemOrange for add-recipe button background

--- apps/mobile/src/screens/RecipesScreen.tsx
+++ apps/mobile/src/screens/RecipesScreen.tsx
@@ -740,7 +740,7 @@ const styles = StyleSheet.create({
     width: 32,
     height: 32,
     borderRadius: 16,
-    backgroundColor: colors.brand,
+    backgroundColor: colors.orange,
     alignItems: 'center',
     justifyContent: 'center',
   },
